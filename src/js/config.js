fb29e6da400021e09d03b176af92f85201ca6558
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-04-22 10:05:22 -0400

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,25 @@
+function Config($stateProvider, $urlRouterProvider){
+
+  $stateProvider
+
+  .state('root', {
+    templateUrl: 'templates/nav.tpl.html'
+  })
+
+  .state('not-found', {
+    url: '/not-found',
+    templateUrl: 'templates/not-found.tpl.html'
+  })
+
+  .state('root.home', {
+    url: '/home',
+    templateUrl: 'templates/home.tpl.html'
+  })
+
+  $urlRouterProvider.when('', '/login');
+      $urlRouterProvider.otherwise('/not-found');
+}
+
+Config.inject = ['$stateProvider', '$urlRouterProvider']
+
+export default Config;
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import angular from 'angular';
+import 'angular-ui-router';
+
+import Config from './config';
+
+
+angular
+.module('app', ['ui.router'])
+  .config(Config)
